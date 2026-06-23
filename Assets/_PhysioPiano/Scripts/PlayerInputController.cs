29096f27b13041d4de410732198601a2a260fa9f
Biel675 <biel675.contato@gmail.com>
2026-06-23 10:35:34 -0300

    Save player's position when swapping scenes

--- Assets/_PhysioPiano/Scripts/PlayerInputController.cs
+++ Assets/_PhysioPiano/Scripts/PlayerInputController.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.XR.CoreUtils;
 
 public class PlayerInputController : MonoBehaviour
 {
+    public static Vector3 PlayerPosition { get; set; } = new(0, 1.5f, -1f);
+    [SerializeField] private GameObject _xrOriginObj;
+    private XROrigin xrOrigin;
+    [SerializeField] private GameObject _mainCamera;
     private const string MENUS_SCENE_NAME = "Menus Scene";
+
+    void Start()
+    {
+        xrOrigin = _xrOriginObj.GetComponent<XROrigin>();
+        xrOrigin.MoveCameraToWorldLocation(PlayerPosition);
+    }
     
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            PlayerPosition = _mainCamera.transform.position;
             SceneManager.LoadScene(MENUS_SCENE_NAME);
         }
     }
